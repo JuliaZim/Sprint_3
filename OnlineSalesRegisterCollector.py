831fc2ecfae06cad17d935118d691c3a84ab47d4
Julia Zimina <krypina2009@yandex.ru>
2025-08-18 10:08:52 +0300

    исправлена логика отлова эксепшена в оставшихся методах

--- OnlineSalesRegisterCollector.py
+++ OnlineSalesRegisterCollector.py
@@ -48,7 +48,7 @@ class OnlineSalesRegisterCollector:
         if name not in self.__item_price:
             raise NameError("Позиция отсутствует в товарном справочнике")
         self.__name_items.append(name)
-        new_number_items = self.number_items  + 1
+        new_number_items = self.number_items + 1
         self.number_items = new_number_items
 
     # 3. Удалить товар из чека
@@ -59,52 +59,41 @@ class OnlineSalesRegisterCollector:
         new_number_items = self.number_items - 1
         self.number_items = new_number_items
 
-
     # 4. Посчитать общую стоимость товара
     def check_amount(self):
         total = []
-        try:
-            for el in self.__name_items:
-                total.append(self.item_price.get(el))
-
-            if len(total) > 10:
-                return sum(total) * 0.9
-            else:
-                return sum(total)
-        except Exception as e:
-            print(e)
+        for el in self.__name_items:
+            total.append(self.item_price.get(el))
+        if len(total) > 10:
+            return sum(total) * 0.9
+        else:
+            return sum(total)
 
     # 5. Вычислить НДС товаров со ставкой 20%
     def twenty_percent_tax_calculation(self):
         twenty_percent_tax = []
         total = []
-        try:
-            for name in self.__name_items:
-                if self.__tax_rate[name] == 20:
-                    twenty_percent_tax.append(name)
-                    total.append(self.item_price.get(name))
-            if len(self.__name_items) > 10:
-                return sum(total) * 0.2 * 0.9
-            else:
-                return sum(total) * 0.2
-        except Exception as e:
-            print(e)
+        for name in self.__name_items:
+            if self.__tax_rate[name] == 20:
+                twenty_percent_tax.append(name)
+                total.append(self.item_price.get(name))
+        if len(self.__name_items) > 10:
+            return sum(total) * 0.2 * 0.9
+        else:
+            return sum(total) * 0.2
 
     # 6. Вычислить НДС товаров со ставкой 10%:
     def ten_percent_tax_calculation(self):
         ten_percent_tax = []
         total = []
-        try:
-            for name in self.__name_items:
-                if self.__tax_rate[name] == 10:
-                    ten_percent_tax.append(name)
-                    total.append(self.item_price.get(name))
-            if len(self.__name_items) > 10:
-                return sum(total) * 0.1 * 0.9
-            else:
-                return sum(total) * 0.1
-        except Exception as e:
-            print(e)
+        for name in self.__name_items:
+            if self.__tax_rate[name] == 10:
+                ten_percent_tax.append(name)
+                total.append(self.item_price.get(name))
+        if len(self.__name_items) > 10:
+            return sum(total) * 0.1 * 0.9
+        else:
+            return sum(total) * 0.1
 
     # 7. Общая сумма налогов
     def total_tax(self):
@@ -115,12 +104,11 @@ class OnlineSalesRegisterCollector:
     # 8. Метод, который возвращает номер телефона
     @staticmethod
     def get_telephone_number(telephone_number):
-            if len(telephone_number) < 10:
-                raise ValueError("Необходимо ввести цифры")
-            if len(telephone_number) > 10:
-                raise ValueError('Необходимо ввести 10 цифр после "+7"')
-            return f"+7{telephone_number}"
-
+        if len(telephone_number) < 10:
+            raise ValueError("Необходимо ввести цифры")
+        if len(telephone_number) > 10:
+            raise ValueError('Необходимо ввести 10 цифр после "+7"')
+        return f"+7{telephone_number}"
 
     # 9. Преобразование даты
     @staticmethod
